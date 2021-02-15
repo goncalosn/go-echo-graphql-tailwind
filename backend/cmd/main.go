86aaafd8b90ac40d665aa8d2edd09d6044f222c0
Gonçalo Nunes <goncalonunes1111@gmail.com>
2021-02-15 02:23:39 +0000

    docker and mongoDB

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/goncalosn/go-echo-graphql-tailwind/internal/db"
 	"github.com/goncalosn/go-echo-graphql-tailwind/pkg/handlers"
 	"github.com/labstack/echo/v4"
 )
@@ -10,5 +11,8 @@ func main() {
 
 	handlers.GetHandlers(e)
 
+	db.Open()
+
 	e.Logger.Fatal(e.Start(":1323"))
+
 }
--- backend/go.mod
+++ backend/go.mod
@@ -4,4 +4,5 @@ go 1.15
 
 require (
 	github.com/labstack/echo/v4 v4.2.0
+	go.mongodb.org/mongo-driver v1.4.6
 )
