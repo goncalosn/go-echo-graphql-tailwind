78aa05c3dedf97bc76e2eded8577593c321fc3a6
Gonçalo Nunes <goncalonunes1111@gmail.com>
2021-03-19 02:52:33 +0000

    configuration for kubernetes deployment

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func init() {
-	err := mgm.SetDefaultConfig(nil, "menu", options.Client().ApplyURI("mongodb://mongo:"+os.Getenv("MONGO_PORT")))
+	err := mgm.SetDefaultConfig(nil, "menu", options.Client().ApplyURI("mongodb://mongodb:"+os.Getenv("MONGO_PORT")))
 
 	if err != nil {
 		log.Fatal(err)
